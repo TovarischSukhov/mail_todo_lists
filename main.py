e0e88a2170c4e2297fb50987623e01f08cd0f766
TovarischSukhov <ks.sukhova@gmail.com>
2018-07-14 14:53:33 +0300

    add amil and logging

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 # coding=utf-8
+import logging.config
+import os
+
+from fact_extraction.setup_parser import TomitaRunner
+from settings import LOGGING_DICT
+
+from views import app
+
+
+
+if __name__ == '__main__':
+    logging.config.dictConfig(LOGGING_DICT)
+    logger = logging.getLogger(__name__)
+    logger.info('Setup parser')
+
+    parser = TomitaRunner(compile_dk=True)
+    parser.setup()
+    logger.info('Parser ready to go!')
+
+    app.run(host='0.0.0.0', port=80, debug=False)
