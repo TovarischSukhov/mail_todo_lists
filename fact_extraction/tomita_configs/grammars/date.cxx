a68cc39e3b2b9aa23ac53dd68e417e25da6db99e
TovarischSukhov <ks.sukhova@gmail.com>
2018-07-14 23:58:14 +0300

    fix

--- fact_extraction/tomita_configs/grammars/date.cxx
+++ fact_extraction/tomita_configs/grammars/date.cxx
@@ -4,7 +4,7 @@ DayOfWeek -> Noun<kwtype="день_недели">;
 Day -> AnyWord<wff=/([1-2]?[0-9])|(3[0-1])/>;
 Month -> Noun<kwtype="month"> | AnyWord<wff=/(0?[1-9])|(1[0-2])/>;
 YearDescr -> "год" | "г. ";
-Year -> AnyWord<wff=/[1-2]?[0-9]{1,3}г?\.?/>;
+Year -> AnyWord<wff=/[1-2]?[0-9]{3}г?\.?/>;
 Year -> Year YearDescr;
 DateSep -> Punct | Comma | Hyphen | '/';
 
@@ -16,8 +16,8 @@ Date -> DayOfWeek interp (DateFact.DayOfWeek) (DateSep)
         (Year interp (DateFact.Year));
 
         // число, месяц и год: "10 января 2011"
-Date -> Day interp (DateFact.Day) (DateSep)
-        Month interp (DateFact.Month) (DateSep)
+Date -> Day interp (DateFact.Day)
+        Month interp (DateFact.Month)
         (Year interp (DateFact.Year));
 
 Date -> Day interp (DateFact.Day) (DateSep)
