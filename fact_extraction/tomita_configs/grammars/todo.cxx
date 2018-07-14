93f150151d84eba9bc311e2414cdd91ee8bdb65d
TovarischSukhov <ks.sukhova@gmail.com>
2018-07-14 19:22:59 +0300

    poor todo

--- fact_extraction/tomita_configs/grammars/todo.cxx
+++ fact_extraction/tomita_configs/grammars/todo.cxx
@@ -1,3 +1,11 @@
 #encoding "utf-8"
 
-S_ -> Adj Noun;
+NeedWord -> 'надо' | 'нужен' | 'нужна';
+
+ToDoWord -> (NeedWord) AnyWord<kwtype='todo'>;
+
+ToDoWord -> NeedWord (AnyWord<kwtype='todo'>) | Verb;
+
+NP -> Noun;
+
+ToDoList -> ToDoWord (Verb) NP interp (CheckListFact.type);
