3efd25412a5ef479536abba8fcdd10e05437836f
TovarischSukhov <ks.sukhova@gmail.com>
2018-07-14 22:00:23 +0300

    gg

--- fact_extraction/tomita_configs/grammars/todo.cxx
+++ fact_extraction/tomita_configs/grammars/todo.cxx
@@ -1,11 +1,21 @@
 #encoding "utf-8"
+#include 'np.cxx'
 
-NeedWord -> 'надо' | 'нужен' | 'нужна';
+NeedWord -> 'надо' | 'нужен' | 'нужный' | 'нужно';
 
 ToDoWord -> (NeedWord) AnyWord<kwtype='todo'>;
 
-ToDoWord -> NeedWord (AnyWord<kwtype='todo'>) | Verb;
+ToDoWord -> NeedWord (AnyWord<kwtype='todo'>);
 
-NP -> Noun;
+//NP -> NP Comma | NP SimConjAnd;
 
-ToDoList -> ToDoWord (Verb) NP+ interp (CheckListFact.type);
+Action_ -> Verb NP;
+//Action_ -> Verb NP Comma NP;
+//Action_ -> Verb NP SimConjAnd NP;
+
+Action -> Action_ interp (CheckListFact.type);
+
+GroupOfActions -> Action SimConjAnd GroupOfActions | Action Comma GroupOfActions | Action;
+
+ToDoList -> ToDoWord NP interp (CheckListFact.type);
+ToDoList -> NeedWord (AnyWord) (AnyWord) GroupOfActions+ ;
