c3b8d40385957e0738ae491cde6a5b531cc9da9b
sergeevii123 <sergeevii123@gmail.com>
2018-07-15 14:40:27 +0300

    get ical in work

--- views.py
+++ views.py
@@ -25,21 +25,25 @@ REALTY_ALL_FIELDS = [
     "встреча"
 ]
 
+predef_messages = [
+    "Привет, к завтрашнему дню надо чтобы ты подготовил презентацию и выложил в папку и скинул ссылку в общий чат. После стэндапа сделаем тестовый прогон.",
+    "Привет, кончилась еда, нужно купить молоко и сыр, пельмени и сметану",
+    "Привет, назначь встречу с Марком Цукербергом на 20-07-2018 19:00",
+    "Привет, назначь встречу с Марком Цукербергом на 2 августа на 10:00"
+]
 
-@app.route('/ping', methods=['GET'])
-def ping():
-    return success_response('YAY!')
+
+@app.route('/message/<id>', methods=['GET'])
+def ping(id):
+    return success_response(predef_messages[int(id)])
 
 
-@app.route('/get_ical/', methods=['POST'])
-@requires_auth
+@app.route('/get_ical', methods=['POST','GET'])
 def get_ical():
-    message = request.args.get('message')
-    if not message:
-        logger.info('No message passed in request.')
-        return error_response('It is required to pass message', STATUSES['HTTP_400_bad_request'])
-    logger.info('Extracting all realty facts from message %s', message)
-    facts = {"facts": {key: val for key, val in extract_facts(message).items() if key in REALTY_ALL_FIELDS}}
+    content = request.get_json(silent=True)
+
+    logger.info('Extracting all realty facts from message %s', content)
+    facts = {"facts": {key: val for key, val in extract_facts(content['message']).items() if key in REALTY_ALL_FIELDS}}
     logger.info('Extracted facts %s', facts)
     events = []
     for fact in facts:
@@ -50,7 +54,7 @@ def get_ical():
             date = datetime.date.today() + datetime.timedelta(days=1)
 
         if fact['time']:
-            time = fact['time'].split(':')
+            time = fact['time'].split('-')
             events.append(create_event(datetime(int(date[2]),int(date[1]),int(date[0]),int(time[0]),int(time[1]),0), fact['action'], fact['checklist'].join("\n")))
 
     return send_file(create_ical(events))
